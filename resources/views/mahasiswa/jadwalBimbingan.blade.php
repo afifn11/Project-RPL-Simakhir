eeb8ddd571087d77a2e75b4e2190c856274396c2
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2024-12-29 18:11:18 +0700

    update color header modal delete

--- resources/views/mahasiswa/jadwalBimbingan.blade.php
+++ resources/views/mahasiswa/jadwalBimbingan.blade.php
@@ -149,7 +149,7 @@
             @csrf
             @method('DELETE')
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header" style="background-color: #FFC7C7">
                     <h5 class="modal-title">Hapus Jadwal Bimbingan</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -159,8 +159,8 @@
                     <p>Apakah Anda yakin ingin menghapus jadwal ini?</p>
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-danger">Hapus</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                    <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Batal</button>
                 </div>
             </div>
         </form>
