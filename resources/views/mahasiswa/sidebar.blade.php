b96141765f52c26b765ca91e2e4501ceede94538
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2025-01-03 23:55:05 +0700

    add link sidebar mahasiswa

--- resources/views/mahasiswa/sidebar.blade.php
+++ resources/views/mahasiswa/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #001f3f; color: #ffffff;">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="{{ route('mahasiswa.dashboard') }}" class="brand-link">
         <img src="{{ asset('assets/dist/img/logo_simakhir.jpeg') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SIMAKHIR</span>
